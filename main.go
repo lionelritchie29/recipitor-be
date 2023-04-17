6e83a8b38c775bf995b601ce93fd35148d633b0a
lionelritchie29 <lionelrtchieee@gmail.com>
2023-04-17 15:14:35 +0700

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	
+	r.GET("/ping", func (c *gin.Context) {
+		c.JSON(200, gin.H {
+			"message": "pong",
+		})
+	})
+	
+	http.ListenAndServe(":3333", r)
+}

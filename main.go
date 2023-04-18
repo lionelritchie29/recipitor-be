af1683c1c16c5c50c20b4e1f44f06ac43d0d3bc3
lionelritchie29 <lionelrtchieee@gmail.com>
2023-04-18 11:19:04 +0700

    feat: add item model and mysql connection with gorm

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/lionelritchie29/recipitor-be/models"
 	"github.com/lionelritchie29/recipitor-be/routes"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
 	godotenv.Load()
+	initDb()
 	router := gin.Default()
 	port := os.Getenv("APP_PORT")
 
@@ -19,8 +24,17 @@ func main() {
 		routes.ItemRoutes(apiRoute)
 	}
 	
-	
-
-	
 	router.Run(port)
+}
+
+func initDb() {
+	dsn := os.Getenv("CONNECTION_STRING")
+  db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	db.AutoMigrate(&models.Item{})
 }
